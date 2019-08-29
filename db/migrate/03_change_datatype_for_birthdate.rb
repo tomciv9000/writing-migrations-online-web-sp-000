8f3dcb77715f7ed8b37d957abd3165beb44da7a0
Tom White <eugene.white@gmail.com>
2019-08-29 01:25:06 +0000

    Automatically backed up by Learn

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,8 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+  
+  def change
+    change_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+  end
+  
+end
